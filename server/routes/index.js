f66dc701b542e29287f84ca74396f3de7095168a
Xavid Ramirez <xavidramirez@AADM08640.local>
2018-08-16 09:11:15 -0500

    linked server and client together with watch

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,10 +2,13 @@ const errors = require('../components/errors');
 const path = require('path');
 
 module.exports = function routes(app, root) {
+  // Add your custom routes here
   app.use('/api/users', require('./users'));
 
+  // Catching paths that are invalid and redirectin to 404 error
   app.route('/:url(api|auth|component|app|assets)/*').get(errors[404]);
 
+  // All Other routes get redirected to the index if they not under /api
   app.route('/*').get((req, res) => {
     res.sendFile(path.join(root, 'dist/index.html'));
   });
